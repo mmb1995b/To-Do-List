8c7041887b877f13d02616c56a60e70785b0832e
Butler <butlemm1@vanderbilt.edu>
2018-10-05 14:31:21 -0500

    TO DO lab

---
+++ app.js
@@ -0,0 +1,41 @@
+//Create references to task elements
+var addButton = document.getElementById("add");
+var taskInput = document.getElementById("task");
+var taskList = document.getElementById("taskList");
+var clearButton = document.getElementById("clear");
+
+//add new item to task list
+addButton.addEventListener("click", function(){
+    var task = taskInput.value;
+    // Don't add an empty string
+    if(task.trim()){
+        // add new task list item
+        var newItem = document.createElement("LI");
+        var taskText = document.createTextNode(task);
+        newItem.appendChild(taskText);
+        // clear text input box
+        taskInput.value = "";
+        //add remove button for new items
+        var removeButton = document.createElement("BUTTON");
+        removeButton.innerHTML = "Done";
+        removeButton.className = "remove";
+        removeButton.addEventListener("click", removeTask);
+        newItem.appendChild(removeButton);
+        taskList.appendChild(newItem);
+    }
+    else{
+       alert("Task cannot be empty");
+    }
+});
+
+// clear entire list
+clearButton.addEventListener("click", function(){
+    taskList.innerHTML = "";
+  });
+
+//remove task from the list
+function removeTask(e){
+    //get the parent list item to remove
+    var taskItem = e.target.parentElement;
+    taskList.removeChild(taskItem);
+}
